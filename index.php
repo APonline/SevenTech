a03e9ed31dcd1eea3f28cf3a31c1b4b7249ea652
Frosty <andrewgphillips74@gmail.com>
2017-06-21 02:52:39 -0400

    first landing

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-echo "the 7 is coming";
-?>
+<style>
+body{
+    background-image: linear-gradient(rgb(255, 255, 255) ,rgb(220, 220, 220));
+    background-position-x: initial;
+    background-position-y: initial;
+    background-size: initial;
+    background-repeat-x: initial;
+    background-repeat-y: initial;
+    background-attachment: initial;
+    background-origin: initial;
+    background-clip: initial;
+    background-color: initial;
+}
+</style>
+
+<div style="height:100%;width:100vw;position:relative;display:table;vertical-align:middle;">
+	<div style="max-width:300px;width:50vw;position:relative;display:table-cell;vertical-align:middle;">
+		<img src='assets/img/se7en-logo.png' width="100%" />
+	</div>
+</div>
