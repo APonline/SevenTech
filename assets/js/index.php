2ec489d88a97d208dbf0908c98590702ef2ac849
Frosty <andrewgphillips74@gmail.com>
2017-07-01 18:23:06 -0400

    big template update

---
+++ assets/js/index.php
@@ -0,0 +1,15 @@
+<style>
+body{
+    background: linear-gradient(to bottom, rgba(90, 0, 0,.7) 0% ,rgba(255, 255, 255, .2) 100%), url(assets/img/wall.png) 50% 50% / 100% no-repeat;
+    background-attachment: initial;
+    background-origin: initial;
+    background-clip: initial;
+    background-color: initial;
+}
+</style>
+
+<div style="height:100%;width:100vw;position:relative;display:table;vertical-align:middle;">
+	<div style="max-width:500px;width:50vw;position:relative;display:table-cell;vertical-align:middle;text-align:center;">
+		<img src='assets/img/se7en-logo.png' width="100%" style="min-width: 500px;max-width: 500px;" />
+	</div>
+</div>
